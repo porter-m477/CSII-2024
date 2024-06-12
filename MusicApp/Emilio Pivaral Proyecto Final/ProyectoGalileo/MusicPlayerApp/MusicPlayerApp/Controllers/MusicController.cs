9083047d6c9259da2c19f2dcfa60e3edf0bad46d
adalepiva <24000816@galileo.edu>
2024-06-12 06:35:08 -0600

    proyecto final

---
+++ MusicApp/Emilio Pivaral Proyecto Final/ProyectoGalileo/MusicPlayerApp/MusicPlayerApp/Controllers/MusicController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using MusicPlayerApp.Data;
+using MusicPlayerApp.Models;
+
+namespace MusicPlayerApp.Controllers
+{
+    public class MusicController
+    {
+        private Database database;
+
+        public MusicController()
+        {
+            database = new Database();
+        }
+
+        public List<Song> GetSongs()
+        {
+            return database.GetSongs();
+        }
+    }
+}
+
---
+++ MusicApp/Emilio Pivaral Proyecto Final/ProyectoGalileo/MusicPlayerApp/MusicPlayerApp/Models/Song.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicPlayerApp.Models
+{
+    public class Song
+    {
+        public int Id { get; set; }
+        public string Cancion { get; set; }
+        public string Artista { get; set; }
+        public string Album { get; set; }
+        public string Genero { get; set; }
+        public string Imagen { get; set; }
+        public string LinkYoutube { get; set; }
+    }
+}
